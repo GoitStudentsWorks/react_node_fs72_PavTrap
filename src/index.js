3a4c3209cca780a06f94aa7176020420da03c399
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-09-09 23:27:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="drink-master-project">
+        <BrowserRouter basename="react_node_fs72_PavTrap">
           <StyledToaster />
           <ScrollToTop />
           <App />
